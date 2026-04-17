b9b65f293449b1edb58f02a15886f948309f8b98
SladkyCitron <57183251+SladkyCitron@users.noreply.github.com>
2026-04-17 11:09:26 +0200

    Update SourceFileMode docs

--- sourceFileMode.go
+++ sourceFileMode.go
@@ -6,9 +6,9 @@ const (
 	// Nop does nothing.
 	Nop SourceFileMode = iota
 
-	// ShortFile produces only the filename (for example main.go:69).
+	// ShortFile produces only the filename (for example main.go:39).
 	ShortFile
 
-	// LongFile produces the full file path (for example /home/user/go/src/myapp/main.go:69).
+	// LongFile produces the full file path (for example /home/user/go/src/myapp/main.go:39).
 	LongFile
 )
